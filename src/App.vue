9b8015a806c050adf3c8972c9dbe3cf6e2006e5f
tobesimple7 <tobesimple7@gmail.com>
2024-09-27 12:26:49 +0900

    test tbsgrid for vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import TbsGrid from './components/TbsGrid.vue'
+
+</script>
+
+<template>
+  <main>
+    <div class="wrapper">
+      <TbsGrid />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/TbsGrid.vue
@@ -0,0 +1,40 @@
+<script>
+import { tbsGridConfigs} from "tbsgrid/dist/tbsgrid-configs.js";
+import { TbsGrid } from "tbsgrid";
+import "tbsgrid/dist/css/tbsgrid.css";
+import { grid_data } from "@/components/tbsgrid_data.js";
+const columns = [
+    {name: "custcd", header: {text: "Code"}, width: 100}
+  , {name: "usernm", header: {text: "Manager"}, width: 100}
+  , {name: "custnm", header: {text: "Customer"}, width: 100}
+  , {name: "bizno", header: {text: "Biz Number"}, width: 100}
+  , {name: "ceo", header: {text: "ceo"}, width: 100}
+  , {name: "tel", header: {text: "Telephone"}, width: 100}
+  , {name: "email", header: {text: "E-Mail"}, width: 100}
+  , {name: "saddr", header: {text: "Area"}, width: 100}
+  , {name: "addr", header: {text: "Address"}, width: 100}
+  , {name: "creditamt", header: {text: "Credit limit"}, type: 'number', scale: '18,2'}
+  , {name: "payamt_d", header: {text: "Other Payable"}, type: 'number', scale: '18,2'}
+  , {name: "getamt_d", header: {text: "Accounts receivable"}, type: 'number', scale: '18,2'}
+  , {name: "useyn", header: {text: "Use"}, width: 60}
+  , {name: "custnote", header: {text: "note"}, width: 100}
+];
+console.dir(tbsGridConfigs);
+export default {
+    name: 'TbsGrid',
+    mounted() {
+      const grid1 = new TbsGrid('grid1', tbsGridConfigs);
+      const options = {};
+      grid1.setGrid(columns, options);
+      grid1.setData(grid_data);
+    }
+}
+</script>
+
+<template>
+  <div id="grid1" style="width: 100%; height: 521px"></div>
+</template>
+
+<style scoped>
+
+</style>
